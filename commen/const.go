4e9a979278a2018359fd7ad216350900f9950f74
11andyxz <andyxiongzheng@gmail.com>
2022-06-09 21:34:58 +0800

    编辑用户注册、登录接口

--- commen/const.go
+++ commen/const.go
@@ -5,13 +5,18 @@ const Success = 0
 //video错误
 const (
 	//样例
-	GetVideoError  = 20001
-	SaveVideoError = 20002
+
+	VideoError = 20001
 )
 
 //user错误
 const (
-	UserNotExistsError = 30001
+	UserError           = 30001
+	UserAlreadyExist    = 30002
+	RegisterFailed      = 30003
+	UserNotExist        = 30004
+	UserCreatError      = 30005
+	UserPasswordMistake = 30006
 )
 
 //评论错误
@@ -31,8 +36,10 @@ const (
 
 //token和parse错误
 const (
-	TokenError = 70001
-	ParseError = 80001
+	TokenError           = 70001
+	ParseError           = 80001
+	PasswordEncryptError = 80002
+	GetTokenError        = 70002
 )
 
 //由于没有开放个人信息编辑功能，所以直接写死，可以自定义图片
--- dao/duser/user.go
+++ dao/duser/user.go
@@ -29,3 +29,37 @@ func GetByToken(token string) *model.User {
 	}
 	return u
 }
+func Create(username, password string) (*model.User, bool) {
+	user := &model.User{
+		Avatar:          commen.Avatar,
+		Signature:       commen.Signature,
+		BackgroundImage: commen.BackgroundImage,
+		UserName:        username,
+		Password:        password,
+		FollowCount:     0,
+		FollowerCount:   0,
+		TotalFavorited:  0,
+		FavoriteCount:   0,
+	}
+	result := database.DB.Create(user)
+	if result.RowsAffected == 0 {
+		return nil, false
+	}
+	return user, true
+}
+func CheckPassword(user model.User, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func GetByName(username string) (*model.User, bool) {
+	user := &model.User{}
+	result := database.DB.Where("UserName=?", username).Find(user)
+	if result.RowsAffected == 0 {
+		return nil, false
+	}
+	return user, true
+}
