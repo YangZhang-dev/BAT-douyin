951459f3c192274580b0ca50d244f49d84b8ca0e
Heartbeat-Lin <1810689947@qq.com>
2022-06-11 11:52:43 +0800

    Update video.go

--- dao/dvideo/video.go
+++ dao/dvideo/video.go
@@ -5,6 +5,7 @@ import (
 	"BAT-douyin/dao/database"
 	"BAT-douyin/model"
 	"fmt"
+	"gorm.io/gorm"
 	"sync"
 )
 
@@ -30,4 +31,79 @@ func Save(path string, userid uint, title string) bool {
 		return false
 	}
 	return true
+}
+
+func LikeVideo(u *model.User, video *model.Video) bool {
+	var m sync.Mutex
+	uid := u.ID
+	vid := video.ID
+	tarVideo := model.FavoriteVideo{}
+	find := database.DB.Where("user_id=? and video_id=?", uid, vid).Find(&tarVideo)
+	if find.RowsAffected != 0 {
+		return false
+	} else {
+		m.Lock()
+		txx := database.DB.Begin()
+		create := txx.Where("user_id=? and video_id=?", uid, vid).Create(&model.FavoriteVideo{UserID: uid, VideoID: vid})
+
+		up1 := txx.Model(&model.Video{ID: vid}).Update("favorite_count", gorm.Expr("favorite_count+?", 1))
+		if up1.RowsAffected == 0 {
+			txx.Rollback()
+			return false
+		}
+		up2 := txx.Model(&model.User{ID: video.AuthorID}).Update("total_favorited", gorm.Expr("total_favorited+?", 1))
+		if up2.RowsAffected == 0 {
+			txx.Rollback()
+			return false
+		}
+		up3 := txx.Model(&model.User{ID: uid}).Update("favorite_count", gorm.Expr("favorite_count+?", 1))
+		if up3.RowsAffected == 0 {
+			txx.Rollback()
+			return false
+		}
+		if create.RowsAffected == 0 {
+			txx.Rollback()
+			return false
+		}
+		m.Unlock()
+		txx.Commit()
+		return true
+	}
+}
+
+func UnlikeVideo(u *model.User, video *model.Video) bool {
+	var m sync.Mutex
+	uid := u.ID
+	vid := video.ID
+	tarVideo := model.FavoriteVideo{}
+	find := database.DB.Where("user_id=? and video_id=?", uid, vid).Find(&tarVideo)
+	if find.RowsAffected == 0 {
+		return false
+	} else {
+		m.Lock()
+		txx := database.DB.Begin()
+		tx := txx.Unscoped().Where("user_id=? and video_id=?", uid, vid).Delete(&model.FavoriteVideo{})
+		if tx.RowsAffected == 0 {
+			txx.Commit()
+			return false
+		}
+		up1 := txx.Model(&model.Video{ID: vid}).Update("favorite_count", gorm.Expr("favorite_count-?", 1))
+		if up1.RowsAffected == 0 {
+			txx.Rollback()
+			return false
+		}
+		up2 := txx.Model(&model.User{ID: video.AuthorID}).Update("total_favorited", gorm.Expr("total_favorited-?", 1))
+		if up2.RowsAffected == 0 {
+			txx.Rollback()
+			return false
+		}
+		up3 := txx.Model(&model.User{ID: uid}).Update("favorite_count", gorm.Expr("favorite_count-?", 1))
+		if up3.RowsAffected == 0 {
+			txx.Rollback()
+			return false
+		}
+		m.Unlock()
+		txx.Commit()
+		return true
+	}
 }
